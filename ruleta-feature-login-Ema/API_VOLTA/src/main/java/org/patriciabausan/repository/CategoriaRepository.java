ecf0c0c00bf7cbf44f20cbd8233dad9f647e14d1
Patricia Bausan Gómez <pbgcole@gmail.com>
2026-05-26 08:57:41 +0200

    Revert "Informe No acabado de documentar"
    This reverts commit a1e5cfb0f5e419a49c4820bfa2532af57163cd40.

--- ruleta-feature-login-Ema/API_VOLTA/src/main/java/org/patriciabausan/repository/CategoriaRepository.java
+++
@@ -1,6 +0,0 @@
-package org.patriciabausan.repository;
-
-import org.patriciabausan.model.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {}
--- ruleta-feature-login-Ema/API_VOLTA/src/main/java/org/patriciabausan/repository/RespuestaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.patriciabausan.repository;
-
-import org.patriciabausan.model.Respuesta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RespuestaRepository extends JpaRepository<Respuesta, Integer> {}
-
