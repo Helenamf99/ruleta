148cbefebb16dc720159bb74f777ad2902dd5249
Helena <helenamacedo1999@gmail.com>
2026-05-28 09:34:34 +0200

    cambios de ayer adaptados al acutal codigo

--- backend/db.js
+++ backend/db.js
@@ -24,8 +24,8 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "TU_PASSWORD",
-  database: "TU_BBDD"
+  password: "root",
+  database: "abp"
 });
 
 connection.connect();
